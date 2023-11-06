de03b857e93f8a55fd3555f24e3b247521ba56b6
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-06 16:57:58 +0300

    todolist-reducer-test.ts todolists-reducer.ts

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,9 @@
+export const todolistsReducer=(state:any,action:any)=>{
+    switch (action.type) {
+        case "XXX":{
+            return
+        }
+        default:
+            return state
+    }
+}

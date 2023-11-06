fbd0a52ab23c56f7090a9eb4ad2a75a17cff3d8f
Evgeniy Klimovch <kliasanik@gmail.com>
2023-11-06 17:07:02 +0300

    TodolistsReducerType

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,11 +1,24 @@
 import {TodolistType} from "../App";
 
-export const todolistsReducer=(state:TodolistType[],action:any)=>{
+export const todolistsReducer = (state: TodolistType[], action: TodolistsReducerType) => {
     switch (action.type) {
-        case "XXX":{
+        case "REMOVE-TODOLIST": {
             return
         }
         default:
             return state
     }
+}
+
+type TodolistsReducerType = TemoveTodolistACType
+type TemoveTodolistACType = ReturnType<typeof removeTodolistAC>
+
+export const removeTodolistAC = (id: string) => {
+    return {
+        type: 'REMOVE-TODOLIST',
+        payload: {
+            id
+        }
+
+    } as const
 }
